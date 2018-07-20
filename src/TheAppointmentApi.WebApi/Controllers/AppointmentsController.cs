2ec13baf3ef3921583e658372a3b9f43871480e0
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-07-20 13:35:58 -0700

    Change WebAPI to full framework

--- src/TheAppointmentApi.WebApi/Controllers/AppointmentsController.cs
+++ src/TheAppointmentApi.WebApi/Controllers/AppointmentsController.cs
@@ -10,13 +10,11 @@ using TheAppointmentApi.WebApi.Controllers.Model;
 namespace TheAppointmentApi.WebApi.Controllers
 {
     [Produces("application/json")]
-    [Route("api/appointments")]
-    public class AppointmentController : Controller
+    [Route("api/Appointments")]
+    public class AppointmentsController : Controller
     {
-        // GET: api/appointment
-        [HttpGet]
-        public IActionResult Get() => NotFound();
-
+        // GET: api/Appointments
+       
         // GET: api/appointment/asasasasas/2018-12-15/free
         [HttpGet("{token}/{date}/free", Name = "GetFreeSlots")]
         public string Get([Required] string token,  [Required] DateTimeOffset? date)
@@ -24,20 +22,14 @@ namespace TheAppointmentApi.WebApi.Controllers
             throw new NotImplementedException();
         }
         
-        // POST: api/appointment
+        // POST: api/Appointments
         [HttpPost("{token}/{date}/{time}/{patientName}")]
         public AppointmentPostResultModel Post([Required] string token,  [Required] DateTimeOffset? date,  [Required] DateTimeOffset? time, [Required] string patientName)
         {
             throw new NotImplementedException();
         }
         
-        //// PUT: api/appointment/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody]string value)
-        //{
-        //}
-        
-        // DELETE: api/appointment/5
+        // DELETE: api/Appointments/5
         [HttpDelete("{token}/{appointmentId}")]
         public void Delete([Required] string token, [Required] string appointmentId)
         {
